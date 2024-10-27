52a127528243854f57f8d20ff8c841d62ab8ef90
frustamov <frustamov@openwaygroup.com>
2024-10-27 11:16:38 +0100

    unused fields removed

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -28,6 +28,5 @@ public class Film {
     @Positive(groups = {RequestMethod.Create.class, RequestMethod.Update.class})
     @Min(value = 1, groups = {RequestMethod.Create.class, RequestMethod.Update.class})
     private int duration;
-    //Collection<Integer> likes;
     // Collection<Director> directors;
 }

b9716150c83e689c48b1dfbbdaad942ac98d05c9
Tras V <103604450+Mellerh@users.noreply.github.com>
2024-10-28 13:14:31 +0300

    add-recommendations работа над фичей. 1.1. Пройдены тесты

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -9,7 +9,6 @@ import ru.yandex.practicum.filmorate.storage.dal.BaseStorage;
 import ru.yandex.practicum.filmorate.storage.dal.dto.FilmDto;
 import ru.yandex.practicum.filmorate.storage.dal.dto.FilmLikesDto;
 import ru.yandex.practicum.filmorate.storage.dal.dto.GenreDto;
-import ru.yandex.practicum.filmorate.storage.dal.dto.UserDto;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -40,6 +39,32 @@ public class FilmDbStorage implements FilmStorage {
             LEFT JOIN rating r ON r.id = f.rating_id
             WHERE f.id = ?
             """;
+    private static final String FIND_SIMILAR_USER_QUERY = "SELECT l.* " +
+            "FROM film_likes AS l " +
+            "WHERE l.film_id IN (" +
+            "SELECT film_id " +
+            "FROM film_likes l1 " +
+            "WHERE l1.user_id = ?) " +
+            "AND l.user_id <> ? " +
+            "GROUP BY l.user_id, l.film_id " +
+            "ORDER BY COUNT(l.film_id) DESC " +
+            "LIMIT 1";
+    private static final String FIND_RECOMMENDED_FILMS_QUERY = """
+            SELECT
+                f.*,
+                g.id as genre_id,
+                g.name as genre_name,
+                r.name as rating_name
+            FROM films f
+            LEFT JOIN film_genres fj ON fj.film_id = f.id
+            LEFT JOIN genre g ON g.id = fj.genre_id
+            LEFT JOIN rating r ON r.id = f.rating_id
+            LEFT JOIN film_likes l ON f.id = l.film_id
+            WHERE l.user_id = ? AND f.id NOT IN (
+                SELECT film_id FROM film_likes WHERE user_id = ?
+            )
+            """;
+
     private static final String INSERT_FILM_QUERY = "INSERT INTO films (rating_id, name, description, release_date, duration)" +
             "VALUES (?, ?, ?, ?, ?)";
     private static final String UPDATE_FILM_QUERY = """
@@ -52,19 +77,16 @@ public class FilmDbStorage implements FilmStorage {
 
     private final BaseStorage<FilmDto> filmBaseStorage;
     private final BaseStorage<FilmLikesDto> filmLikesBaseStorage;
-    private final BaseStorage<UserDto> userDtoBaseStorage;
 
 
     @Autowired
     public FilmDbStorage(
             JdbcTemplate jdbc,
             RowMapper<FilmDto> filmDtoRowMapper,
-            RowMapper<FilmLikesDto> filmLikesRowMapper,
-            RowMapper<UserDto> userDtoRowMapper
+            RowMapper<FilmLikesDto> filmLikesRowMapper
     ) {
         filmBaseStorage = new BaseStorage<>(jdbc, filmDtoRowMapper);
         filmLikesBaseStorage = new BaseStorage<>(jdbc, filmLikesRowMapper);
-        userDtoBaseStorage = new BaseStorage<>(jdbc, userDtoRowMapper);
     }
 
     @Override
@@ -167,110 +189,22 @@ public class FilmDbStorage implements FilmStorage {
         return outputFilms.values();
     }
 
-
-//    public Collection<FilmDto> getUserRecommendations(Long userId) {
-//        // SQL-запрос для поиска ID пользователя с наибольшим количеством совпадений по лайкам
-//        String findSimilarUserQuery = "SELECT l1.user_id " +
-//                "FROM film_likes l1 " +
-//                "JOIN film_likes l2 ON l1.film_id = l2.film_id " +
-//                "GROUP BY l1.user_id " +
-//                "ORDER BY COUNT(l1.film_id) DESC " +
-//                "LIMIT 1";
-//
-//        // Получаем ID пользователя, который имеет наибольшее количество совпадений по лайкам с текущим пользователем
-//        Optional<Long> similarUserOptional = filmLikesBaseStorage
-//                .findOne(findSimilarUserQuery, userId, userId)
-//                .map(FilmLikesDto::getUserId);
-//
-//        if (similarUserOptional.isEmpty()) {
-//            return Collections.emptyList();
-//        }
-//
-//        Long similarUserId = similarUserOptional.get();
-//
-//        // SQL-запрос для получения фильмов, которые понравились похожему пользователю, но не текущему
-//        String recommendedFilmsQuery = "SELECT f.id, f.name, f.description, f.release_date, " +
-//                "f.duration, r.id AS rating_id, r.name AS rating_name " +
-//                "FROM films f " +
-//                "JOIN film_likes l ON f.id = l.film_id " +
-//                "LEFT JOIN rating r ON f.rating_id = r.id " +
-//                "WHERE l.user_id = ? AND f.id NOT IN (" +
-//                "   SELECT film_id FROM film_likes WHERE user_id = ?" +
-//                ")";
-//
-//        // Получаем список фильмов, которые понравились похожему пользователю, но отсутствуют у текущего пользователя
-//        Collection<FilmDto> recommendedFilms = filmBaseStorage.findMany(recommendedFilmsQuery, similarUserId, userId);
-//
-//        return prepareFilmDtoData(recommendedFilms);
-//    }
-
     public Collection<FilmDto> getUserRecommendations(Long userId) {
-        // Получаем всех пользователей и их лайки
-        Collection<FilmLikesDto> allLikes = filmLikesBaseStorage.findMany("SELECT * FROM film_likes");
-        log.info("размер allLikes " + allLikes.size());
-        // Группируем лайки по пользователям
-        Map<Long, Set<Long>> userLikes = new HashMap<>();
-        for (FilmLikesDto like : allLikes) {
-            userLikes.computeIfAbsent(like.getUserId(), k -> new HashSet<>()).add(like.getFilmId());
-        }
+        Optional<Long> similarUserOptional = filmLikesBaseStorage
+                .findOne(FIND_SIMILAR_USER_QUERY, userId, userId)
+                .map(FilmLikesDto::getUserId);
 
-        // Получаем лайки текущего пользователя
-        Set<Long> currentUserLikes = userLikes.getOrDefault(userId, Collections.emptySet());
-        log.info("размер currentUserLikes " + currentUserLikes.size());
-
-        // Находим пользователей с максимальным количеством пересечений
-        Map<Long, Integer> similarUsers = new HashMap<>();
-        for (Map.Entry<Long, Set<Long>> entry : userLikes.entrySet()) {
-            Long otherUserId = entry.getKey();
-            if (!otherUserId.equals(userId)) {
-                Set<Long> likes = entry.getValue();
-                // Находим пересечения лайков
-                int intersectionCount = (int) likes.stream().filter(currentUserLikes::contains).count();
-                if (intersectionCount > 0) {
-                    similarUsers.put(otherUserId, intersectionCount);
-                }
-            }
-        }
-
-        // Находим пользователя с максимальным количеством пересечений
-        Long mostSimilarUserId = similarUsers.entrySet().stream()
-                .max(Map.Entry.comparingByValue())
-                .map(Map.Entry::getKey)
-                .orElse(null);
-        log.info("размер mostSimilarUserId " + mostSimilarUserId);
-
-        if (mostSimilarUserId == null) {
+        if (similarUserOptional.isEmpty()) {
             return Collections.emptyList();
         }
+        Long similarUserId = similarUserOptional.get();
 
-        // Получаем фильмы, которые пролайкал похожий пользователь
-        Set<Long> similarUserLikes = userLikes.get(mostSimilarUserId);
-        log.info("размер similarUserLikes " + similarUserLikes.size());
-        // Исключаем фильмы, которые лайкнул текущий пользователь
-        similarUserLikes.removeAll(currentUserLikes);
-        log.info("ошибка до removeAll");
+        // Получаем список фильмов, которые понравились похожему пользователю, но отсутствуют у текущего пользователя
+        Collection<FilmDto> recommendedFilms = filmBaseStorage.findMany(FIND_RECOMMENDED_FILMS_QUERY, similarUserId, userId);
 
-        /// TODO ошибка в запросе либо DTO мапере
-        // Получаем детали фильмов, которые можно рекомендовать
-        Collection<FilmDto> recommendedFilms = filmBaseStorage.findMany(
-                "SELECT f.id, f.name, f.description, f.release_date, f.duration, r.id AS rating_id, r.name AS rating_name " +
-                        "FROM films f " +
-                        "LEFT JOIN rating r ON f.rating_id = r.id " +
-                        "WHERE f.id IN (" +
-                        String.join(",", similarUserLikes.stream().map(String::valueOf).toArray(String[]::new)) + ")"
-        );
-
-
-        return recommendedFilms;
+        return prepareFilmDtoData(recommendedFilms);
     }
+
 
 }
 
-
-//SELECT l1.user_id
-//FROM film_likes l1
-//JOIN film_likes l2 ON l1.film_id = l2.film_id
-//WHERE l2.user_id = 30 AND l1.user_id <> 30
-//GROUP BY l1.user_id
-//ORDER BY COUNT(l1.film_id) DESC
-//LIMIT 1;

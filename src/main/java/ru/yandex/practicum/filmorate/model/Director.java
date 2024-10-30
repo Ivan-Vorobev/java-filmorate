2389ad35a6b882fef3ede164dd814713878226b0
soklenxx <soklenx@yandex.ru>
2024-10-30 22:53:13 +0300

    fix Director add check not blank field name

--- src/main/java/ru/yandex/practicum/filmorate/model/Director.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -1,12 +1,15 @@
 package ru.yandex.practicum.filmorate.model;
 
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
+import ru.yandex.practicum.filmorate.RequestMethod;
 
 @Data
 @Builder
 public class Director {
     private Long id;
+    @NotBlank(groups = {RequestMethod.Create.class, RequestMethod.Update.class})
     private String name;
 }
